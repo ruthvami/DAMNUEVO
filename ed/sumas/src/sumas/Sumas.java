3c47a7de08d7035421ef7124a60e02ecb58f67f6
ruthvami <ruthvami@gmail.com>
2023-01-23 21:47:19 +0100

    h

---
+++ ed/sumas/src/sumas/Sumas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sumas;
+
+/**
+ *
+ * @author rutvac
+ */
+public class Sumas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int suma=0;
+        for (int i = 1; i <=100; i++) {
+            suma+=i;
+            
+        }
+        System.out.println(suma);
+    }
+    
+}
