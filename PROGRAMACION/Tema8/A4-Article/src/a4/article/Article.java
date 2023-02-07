d03225b9fab8e35e5a25e49f11505ba655a1585a
Ruth Vacas <ruthvami@gmail.com>
2023-02-07 12:58:50 +0100

    erroe en 11

--- PROGRAMACION/Tema8/A4-Article/src/a4/article/Article.java
+++ PROGRAMACION/Tema8/A4-Article/src/a4/article/Article.java
@@ -13,28 +13,29 @@ public class Article {
     private String nom;
     private double preu;
     private double iva;
-    private int existencies;
-    private int stock;
-    private int quantsQueden=getExistencies() / getStock();
-    public Article(String nom, double preu, double iva,int quantsQueden) {
-        if (!(preu < 0 || iva < 0)) {
-            this.nom = nom;
+    private int quantsQueden;
+//Solo se asigna un valor al constructor si se cumple que el precio es positivo,
+//si el iva es o 10,21 o 4 y si la cantidad de stock es positiva
+
+    public Article(String nom, double preu, double iva, int quantsQueden) {
+        this.nom = nom;
+        if (preu >= 1) {
             this.preu = preu;
-            this.iva = iva;
-            
-            System.out.println("Datos correctos");
-
         } else {
-            this.nom = null;
-            this.preu = 0;
-            this.iva = 0;
-
-            System.out.println("*** ERROR al introducir los datos ");
-
+            System.out.println("*** ERROR: al introducir le precio");
         }
-    }
+        if (iva == 10 || iva == 21 || iva == 4) {
+            this.iva = iva;
+        } else {
+            System.out.println("*** ERROR: al introducir el IVA");
+        }
+        if (quantsQueden >= 0) {
+            this.quantsQueden = quantsQueden;
+        } else {
+            System.out.println("*** ERROR: el stock no es correcto");
+        }
 
-    
+    }
 
     public String getNom() {
         return nom;
@@ -48,72 +49,74 @@ public class Article {
         return iva;
     }
 
-    public int getExistencies() {
-        return existencies;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-    public int getQuantsQueden(){
+    public int getQuantsQueden() {
         return quantsQueden;
     }
+//para calcular el IVA se multiplica el precio por el iva mas cien y esto dividido entre 100
+
     public double getPVP() {
-        return (getPreu() * (1 + getIva()));
+        return (this.preu * ((this.iva + 100) / 100));
     }
-    public double getPVPDescompte( double per) {
-        return (getPVP()*(1-per/100));
+//Para  calcular el descunto se resta el precio del iva  a el porcentaje de descuento
+//dividido entre 100 y se multiplica por el iva
+
+    public double getPVPDescompte(double per) {
+        return (this.getPVP() - ((per / 100) * this.getPVP()));
     }
-    public boolean vendre( int prenda) {
-        if(getQuantsQueden()>0){
+//Si el stock es mayor que el numero introducido se le resta al stock el numero introducido
+
+    public boolean vendre(int prenda) {
+        if (this.quantsQueden > prenda) {
+            this.quantsQueden -= prenda;
             return true;
-        }else{
-        return false;
+        } else {
+            this.quantsQueden = 0;
+            return false;
         }
     }
-    public boolean emmagatzema(int per ) {
-        int cons=getStock()+per;
-        if(cons>0){
+//devuelve una suma del porcentaje junto con el stock si se suma lo introducido mas stock
+
+    public boolean emmagatzema(int per) {
+        if (per + this.quantsQueden > 0) {
+            this.quantsQueden += per;
             return true;
-        }else{
-        return false;
+        } else {
+            return false;
         }
+
     }
-    public void imprimeix(){
-        System.out.println(getNom()+" - Precio: "+getPreu()+"€"+"- IVA: "+(getIva()*100)+"% - PVP: "+(getPVP())+" -Quedan: "+getStock());
+    public void imprimeix() {
+        System.out.println(getNom() + " - Precio: " + getPreu() + "€" + "- IVA: " + getIva() + "% - PVP: " + getPVP() + " -Quedan: " + getQuantsQueden());
     }
+
     public void setNom(String nom) {
 
         this.nom = nom;
     }
 
     public void setPreu(double preu) {
-        if ((preu < 0)) {
-            this.preu = 0;
-            System.out.println("*** ERROR al introducir los datos ");
+        if ((preu <= 0)) {
+
+            System.out.println("*** ERROR al introducir el precio ");
         } else {
             this.preu = preu;
         }
     }
 
-    public void setIva(double iva) {
-        if ((iva < 0)) {
-            this.iva = 0;
-            System.out.println("*** ERROR al introducir los datos ");
-        } else {
+    public void setIva(int iva) {
+        if ((iva == 10 || iva == 21 || iva == 4)) {
             this.iva = iva;
-
+        } else {
+            this.iva = 0;
+            System.out.println("*** ERROR al introducir el IVA");
         }
     }
 
-    public void setExistencies(int existencies) {
-        this.existencies = existencies;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
     public void setQuantsQueden(int queden) {
-        this.quantsQueden = queden;
+        if (quantsQueden >= 0) {
+            this.quantsQueden = queden;
+        } else {
+            System.out.println("*** ERROR al introducir el stock ");
+        }
     }
 }
--- PROGRAMACION/Tema8/a/src/a/A.java
+++ PROGRAMACION/Tema8/a/src/a/A.java
@@ -1,39 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package a;
+
+import java.util.Scanner;
 
 /**
  *
  * @author ruthv
  */
 public class A {
-class Article {
-  String nom;
-  double preu;
-  final double IVA = 0.21;
-  int quantitat;
 
-  public Article(String nom, double preu, int quantitat) {
-    this.nom = nom;
-    this.preu = preu;
-    this.quantitat = quantitat;
-  }
-
-  public double getPreuIVA() {
-    return preu * (1 + IVA);
-  }
-}
 
 public class Main {
-  public  void main(String[] args) {
-    Article pijama = new Article("Pijama", 10, 5);
 
-    System.out.println(pijama.nom + " - Preu: " + pijama.preu + "€ - IVA: 21% - PVP: " + pijama.getPreuIVA() + "€");
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
 
-    pijama.preu = 15;
+      System.out.print("Introduce el importe: ");
+      double importe = input.nextDouble();
 
-    System.out.println(pijama.nom + " - Preu: " + pijama.preu + "€ - IVA: 21% - PVP: " + pijama.getPreuIVA() + "€");
-  }
+      System.out.print("Introduce el porcentaje de IVA: ");
+      double porcentajeIVA = input.nextDouble();
+
+      double resultado = importe * (porcentajeIVA / 100);
+
+      System.out.println("El IVA es: " + resultado);
+   }
 }}
