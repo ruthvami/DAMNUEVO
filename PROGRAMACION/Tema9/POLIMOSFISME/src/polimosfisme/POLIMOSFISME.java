577f5328fef02e5f5d5fb2746975143a1c2dc69d
ruthvami <ruthvami@gmail.com>
2023-02-15 20:13:08 +0100

    errrr

--- PROGRAMACION/Tema9/POLIMOSFISME/src/polimosfisme/POLIMOSFISME.java
+++ PROGRAMACION/Tema9/POLIMOSFISME/src/polimosfisme/POLIMOSFISME.java
@@ -13,44 +13,46 @@ public class POLIMOSFISME {
     /**
      * @param args the command line arguments
      */
-    
-
     public static void main(String[] args) {
         // TODO code application logic here
-        Mare mare1=new Mare();
-        Filla1 fi1=new Filla1();
-        Filla2 fi2=new Filla2();
-         System.out.println("Variable mare1: ");
+        Mare mare1 = new Mare();
+        Filla1 fi1 = new Filla1();
+        Filla2 fi2 = new Filla2();
+        System.out.println("Variable mare1: ");
         mare1.crida();
-        
+
         Mare mare2;
-        mare2=mare1;
+        mare2 = mare1;
         System.out.println("Variable mare2: ");
         mare2.crida();
-        
-        mare2=fi1;
+
+        mare2 = fi1;
         System.out.println("Variable mare2: ");
         mare2.crida();
-        
-        mare2=fi2;
+
+        mare2 = fi2;
         System.out.println("Variable mare2: ");
         mare2.crida();
     }
 }
- class Mare {
-        public void crida(){
-            System.out.println("Estic a la Mare");
-        }
-    }
 
-     class Filla1 extends Mare {
-        public void crida(){
-            System.out.println("Estic a la Filla1");
-        }
-    }
+class Mare {
 
-     class Filla2 extends Mare {
-        public void crida(){
-            System.out.println("Estic a la Filla2");
-        }
+    public void crida() {
+        System.out.println("Estic a la Mare");
     }
+}
+
+class Filla1 extends Mare {
+
+    public void crida() {
+        System.out.println("Estic a la Filla1");
+    }
+}
+
+class Filla2 extends Mare {
+
+    public void crida() {
+        System.out.println("Estic a la Filla2");
+    }
+}
