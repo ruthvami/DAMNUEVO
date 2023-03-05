189f857aa645e4f6c6d9e1a4877ed084b125ff46
Ruth Vacas <ruthvami@gmail.com>
2023-03-05 17:29:04 +0100

    asd

---
+++ PROGRAMACION/Tema9/E4_Banc/src/e4_banc/CompteBancari.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package e4_banc;
+
+/**
+ *
+ * @author ruthv
+ */
+public class CompteBancari {
+    private String IBAN;
+    private double saldo;
+
+    public CompteBancari(String IBAN, double saldo) {
+        this.IBAN = IBAN;
+        this.saldo = saldo;
+    }
+    public double
+}
