508e3c0a372f115a61f5fbed998b1d372fe7935d
ruthvami <ruthvami@gmail.com>
2023-03-14 18:15:09 +0100

    qwe

---
+++ LM/hola/xslt.xslt
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet >
+
+   
+
+    <xsl:template match="/" >
+        <xsl:sequence select="3"/>
+    
+    </xsl:template>
+
+    
+
+</xsl:stylesheet>
--- PROGRAMACION/Tema10/ejemplosexcepciones/src/ejemplosexcepciones/Ejemplosexcepciones.java
+++ PROGRAMACION/Tema10/ejemplosexcepciones/src/ejemplosexcepciones/Ejemplosexcepciones.java
@@ -1,5 +1,8 @@
 package ejemplosexcepciones;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author rutvac
@@ -28,12 +31,36 @@ public class Ejemplosexcepciones {
 //        System.out.println("El numero es " + num);
 //
 ////3
-          int v[]={1,2,3};
-          int elem;
-          
-          elem=v[5];
-          
-          System.out.println("El elemento es "+elem);
+//          int v[]={1,2,3};
+//          int elem;
+//          
+//          elem=v[5];
+//          
+//          System.out.println("El elemento es "+elem);
+        int div, x, y;
+        Scanner sc = new Scanner(System.in);
+        int v[] = {1, 2, 3,};
+
+        try {
+            System.out.println("Introduce el valor del nominador");
+            x = sc.nextInt();
+            System.out.println("Introduce el valor del denominador");
+            y = sc.nextInt();
+            div = x / y;
+            System.out.println("El resultado es "+div );
+            System.out.println("Que posicion quieres enseñar");
+            System.out.println("El numero es "+v[sc.nextInt()]);
+
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir entre 0");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("El numero introducido esta fuera del rango");
+        } catch (InputMismatchException e) {
+            System.out.println("No se puede añadir un valor diferente a un int");
+        } finally {
+            System.out.println("Fin del programa");
+        }
+
     }
 
 }
