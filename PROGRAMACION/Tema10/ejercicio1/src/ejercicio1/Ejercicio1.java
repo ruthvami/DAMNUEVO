300ba5c8332b7fc94133185600494ca17ec3156e
ruthvami <ruthvami@gmail.com>
2023-03-24 20:10:53 +0100

    asd882

--- PROGRAMACION/Tema10/ejercicio1/src/ejercicio1/Ejercicio1.java
+++ PROGRAMACION/Tema10/ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -46,26 +46,49 @@ public class Ejercicio1 {
 //            System.out.println("***No se puede dividi entre 0");
 //        }
 // ejercicio3
-        double v[] = new double[5];
-        
-
-        for (int i = 0; i < v.length; i++) {
+//        double v[] = new double[5];
+//        
+//
+//        for (int i = 0; i < v.length; i++) {
+//            try {
+//                System.out.println("Introduce un valor: ");
+//                v[i] = sc.nextDouble();
+//
+//            } catch (InputMismatchException e) {
+//                System.out.println("Error al introducir un numero");
+//                sc.next();
+//                i--;
+//
+//            }
+//
+//        }
+//        mostrar(v);
+//ejercicio4
+        int MIN = 1, MAX1 = 10, MAX2 = 100, pos;
+        int N = MIN + (int) (Math.random() * (MAX1 - MIN + 1));
+        int[] vector = new int[N];
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = MIN + (int) (Math.random() * (MAX2 - MIN + 1));
+        }
+        for (int i = 0; i < vector.length; i++) {
             try {
-                System.out.println("Introduce un valor: ");
-                v[i] = sc.nextDouble();
-
+                System.out.println("Que posicion quieres enseñar: (array:"+vector.length+")");
+                pos = sc.nextInt();
+                if (pos < 0) {
+                  
+                }
+                System.out.println(vector[i]);
             } catch (InputMismatchException e) {
                 System.out.println("Error al introducir un numero");
-                sc.next();
-                i--;
-
             }
+//catch ( e) {
+//                System.out.println("Error al introducir un numero");
+//            }
 
         }
-        mostrar(v);
     }
 
-    public static void mostrar(double v[]) {
-        System.out.println(Arrays.toString(v));
-    }
+//    public static void mostrar(double v[]) {
+//        System.out.println(Arrays.toString(v));
+//    }
 }
