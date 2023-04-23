c4844c105a1277a89c84faf47cf115d99f323afd
Ruth Vacas <ruthvami@gmail.com>
2023-04-23 22:13:42 +0200

    hoy

--- PROGRAMACION/Tema11/EJ1-CONTROLD'ACCES/src/ej1/controld/acces/EJ1CONTROLDACCES.java
+++ PROGRAMACION/Tema11/EJ1-CONTROLD'ACCES/src/ej1/controld/acces/EJ1CONTROLDACCES.java
@@ -4,6 +4,8 @@
  */
 package ej1.controld.acces;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -19,15 +21,12 @@ public class EJ1CONTROLDACCES {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+//        //ejercicio1
         Map< String, String> m = new HashMap<>();
         m.put("hola", "HOLA");
-        m.put("Contraseña", "estanoes");
+        m.put("RUTVAC", "estanoes");
 
         Scanner sc = new Scanner(System.in);
-//        System.out.println("Introduce un usuario:");
-//        String x = sc.nextLine();
-//        System.out.println("Introduce una contraseña:");
-//        String y = sc.nextLine();
         boolean bien = true;
         int i = 0;
         do {
@@ -43,7 +42,7 @@ public class EJ1CONTROLDACCES {
                 System.out.println("Error al acceder el usuario");
                 i++;
                 System.out.println("Te quedan " + (3 - i) + " intentos");
-            }else if (!m.containsValue(y)) {
+            } else if (!m.containsValue(y)) {
                 System.out.println("Error al acceder la contraseña");
                 i++;
                 System.out.println("Te quedan " + (3 - i) + " intentos");
@@ -54,6 +53,50 @@ public class EJ1CONTROLDACCES {
 
         } while (bien);
 
+//    }
+////ejercicio2
+//        Map<String, String> m = new HashMap<>();
+//        m.put("hello", "hola");
+//        m.put("goodbye", "adiós");
+//        m.put("thank you", "gracias");
+//        m.put("please", "por favor");
+//        m.put("sorry", "lo siento");
+//        m.put("yes", "sí");
+//        m.put("no", "no");
+//        m.put("how are you?", "¿cómo estás?");
+//        m.put("I'm fine, thanks", "estoy bien, gracias");
+//        m.put("what's your name?", "¿cómo te llamas?");
+//        m.put("my name is...", "mi nombre es...");
+//        m.put("where are you from?", "¿de dónde eres?");
+//        m.put("I'm from...", "soy de...");
+//        m.put("what time is it?", "¿qué hora es?");
+//        m.put("I don't understand", "no entiendo");
+//        m.put("can you help me?", "¿puedes ayudarme?");
+//        m.put("where is the bathroom?", "¿dónde está el baño?");
+//        m.put("how much does it cost?", "¿cuánto cuesta?");
+//        m.put("I would like...", "me gustaría...");
+//        m.put("do you speak English?", "¿hablas inglés?");
+//        m.put("I don't speak Spanish", "no hablo español");
+//        m.put("good luck", "buena suerte");
+//        int cor = 0;
+//        ArrayList<String> pre = new ArrayList<>(m.keySet());
+//        Collections.shuffle(pre);
+//
+//        for (int i = 1; i <= 5; i++) {
+//
+//            int x = 1 + (int) (Math.random() * (22 - 1 + 1));
+//            String pregunta = pre.get(i);
+//            System.out.println(i + "- Introduce la traduccion de " + pregunta);
+//            String resp = sc.nextLine();
+//
+//            if (resp.equals(m.get(pre))) {
+//                System.out.println("Correcte!");
+//                cor++;
+//            } else {
+//                System.out.println("Resposta incorrecta. La resposta correcta és " + m.get(pregunta));
+//            }
+//
+//        }
+//        System.out.println("Has tingut " + cor + " resposta/es correcta/es");
     }
-
 }
