7c662321116d94febdafd89f47a673394c15cb22
Ruth Vacas <ruthvami@gmail.com>
2023-05-02 13:02:53 +0200

    nuevos

--- PROGRAMACION/Tema11/A3/src/a3/A3.java
+++ PROGRAMACION/Tema11/A3/src/a3/A3.java
@@ -17,28 +17,28 @@ public class A3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        File a1 = new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/Documents");
-        File foto = new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/Fotografies");
-        File llibre = new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/Llibres");
+        File a1 = new File("C:/Users/ruthv/Documents/DAMNUEVO/PROGRAMACION/Tema11/A3/Documents");
+        File foto = new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/Fotografies");
+        File llibre = new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/Llibres");
+        System.out.println(a1.exists());
 
-        a1.renameTo(new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS"));
-        foto.renameTo(new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/FOTOS"));
-        llibre.renameTo(new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/LECTURES"));
+        a1.renameTo(new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS"));
+        foto.renameTo(new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/FOTOS"));
+        llibre.renameTo(new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/LECTURES"));
 //        System.out.println(foto.exists());
-        cambiarNombres(new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/FOTOS"));
-        cambiarNombres(new File("/DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/LECTURES"));
+        cambiarNombres(new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/FOTOS"));
+        cambiarNombres(new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A3/DOCS/LECTURES"));
     }
 
     public static void cambiarNombres(File carpeta) {
         // Obtener los archivos de la carpeta
         File[] archivos = carpeta.listFiles();
-        for (File archivo : archivos) {
-            System.out.println(archivo);
-        }
+        
         for (File archivo : archivos) {
             String nombre = archivo.getName().replaceFirst("[.][^.]+$", "");
 
             archivo.renameTo(new File(archivo.getParent(), nombre));
         }
+        
     }
 }
---
+++ PROGRAMACION/Tema11/A4/src/a4/A4.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a4;
+
+import java.io.File;
+
+/**
+ *
+ * @author ruthv
+ */
+public class A4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        File carp1 = new File("C:/Users/ruthv/Documents/DAMNUEVO/PROGRAMACION/Tema11/A4/Documents/LesMeuesCoses");
+        File carp2 = new File("C:/Users/ruthv/Documents/DAMNUEVO/PROGRAMACION/Tema11/A4/Documents/Alfabet");
+        carp1.mkdir();
+        carp2.mkdir();
+        File foto = new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A4/Documents/Fotografies");
+        File llibre = new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A4/Documents/Llibres");
+        foto.renameTo(new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A4/Documents/LesMeuesCoses/Fotografies"));
+        llibre.renameTo(new File("C:/Users/ruthv/Documents//DAMNUEVO/PROGRAMACION/Tema11/A4/Documents/LesMeuesCoses/Llibres"));
+
+        for (char letra = 'A'; letra <= 'Z'; letra++) {
+            String valor=String.valueOf(letra);
+            File carpet=new File("C:/Users/ruthv/Documents/DAMNUEVO/PROGRAMACION/Tema11/A4/Documents/Alfabet/");
+            File hola=new File(carpet.getPath(),valor);
+            hola.mkdir();
+        }
+    }
+
+}
