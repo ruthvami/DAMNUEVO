9f466bd313eec375a7b2ff8b633f9caf34b7fe94
ruthvami <ruthvami@gmail.com>
2023-05-26 20:19:13 +0200

    asd

--- PROGRAMACION/Tema13/EJemplo1/src/ejemplo1/EJemplo1.java
+++ PROGRAMACION/Tema13/EJemplo1/src/ejemplo1/EJemplo1.java
@@ -21,10 +21,21 @@ public class EJemplo1 {
             // TODO code application logic hereString url = "jdbc:mysql://localhost:3306/prova";
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             String url = "jdbc:mysql://localhost:3306/prova";
+
             Connection conn = DriverManager.getConnection(url, "root", "");
-            Statement stmt =conn.createStatement();
-            conn.close();
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM venedors");
+            
+            while(rs.next()){
+                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getInt(4));
+            }
+            rs.close();
             stmt.close();
+            conn.close();
+            
+            
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
