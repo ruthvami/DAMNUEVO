26df8d9f18b542b55f6e3e0e86bb7de12a4d70cc
ruthvami <ruthvami@gmail.com>
2023-05-29 20:12:48 +0200

    asd

--- PROGRAMACION/Tema13/EJemplo1/src/ejemplo1/EJemplo1.java
+++ PROGRAMACION/Tema13/EJemplo1/src/ejemplo1/EJemplo1.java
@@ -24,18 +24,29 @@ public class EJemplo1 {
 
             Connection conn = DriverManager.getConnection(url, "root", "");
 
-            Statement stmt = conn.createStatement();
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 
             ResultSet rs = stmt.executeQuery("SELECT * FROM venedors");
-            
-            while(rs.next()){
-                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getInt(4));
-            }
+
+//            while(rs.next()){
+//                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getInt(4));
+//            }
+//------------------------
+//            rs.last();
+//            rs.updateString("Nom", "Kill Bill");
+//            rs.updateRow();
+//------------------------
+//            rs.moveToInsertRow();
+//            rs.updateString(2, " Kull Bill");
+//            rs.updateInt(4, 156142684);
+//            rs.insertRow();
+//            rs.moveToCurrentRow();
+            rs.absolute(3);
+            rs.deleteRow();
             rs.close();
             stmt.close();
             conn.close();
-            
-            
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
