6d5f0951b9c649350a5fda90fe197f94e18e42be
Ruth Vacas <ruthvami@gmail.com>
2023-06-03 15:10:58 +0200

    asd

--- PROGRAMACION/Tema13/Exercici 1 – Magatzem/src/exercici/pkg1/magatzem/InterficieBBDD.java
+++ PROGRAMACION/Tema13/Exercici 1 – Magatzem/src/exercici/pkg1/magatzem/InterficieBBDD.java
@@ -12,53 +12,147 @@ import java.sql.*;
  */
 class InterficieBBDD {
 
-    private Connection conn;
+    private final Connection conn;
+    private ResultSet prod;
+    private ResultSet tot;
 
-    public InterficieBBDD(Connection conn) throws SQLException {
+    /**
+     * Constructor de la clase InterficieBBDD.
+     *
+     * @param conn conexión a la base de datos.
+     */
+    public InterficieBBDD(Connection conn) {
         this.conn = conn;
     }
 
-    Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+    /**
+     * Calcula la suma de los precios de los productos de magatzem.
+     *
+     * @param bien Indica si se deben considerar todos los productos o solo los
+     * activos.
+     * @throws SQLException Si ocurre un error al acceder a la base de datos.
+     * @return La suma de los precios de los productos.
+     */
+    public int suma(boolean bien) {
+        int sum = 0;
+        try {
+            Statement stmt = conn.createStatement();
+            prod = stmt.executeQuery("SELECT * FROM productes");
 
-    ResultSet prod = stmt.executeQuery("SELECT * FROM productes");
-    ResultSet tot = stmt.executeQuery("SELECT * FROM totals");
+            if (bien == false) {
+                while (prod.next()) {
+                    if (prod.getInt(4) == 1) {
+                        sum += prod.getInt("preu");
+                    }
+                }
+            } else {
+                while (prod.next()) {
+                    sum += prod.getInt("preu");
+                }
+            }
 
-    public double suma(boolean bien) throws SQLException {
-        double sum = 0;
-        if (bien == false) {
+        } catch (SQLException e) {
+            System.out.println("\nError al introduir la base de dades: " + e.getMessage());
+        }
+        return sum;
+    }
+
+    /**
+     * Calcula la suma de los precios de los productos de magatzem.
+     *
+     * @return La suma de los precios de los productos activos.
+     * @throws SQLException Si ocurre un error al acceder a la base de datos.
+     */
+    public int suma() {
+        int sum = 0;
+        try {
+            Statement stmt = conn.createStatement();
+            prod = stmt.executeQuery("SELECT * FROM productes");
+
             while (prod.next()) {
                 if (prod.getInt(4) == 1) {
                     sum += prod.getInt("preu");
                 }
             }
-        } else {
-            while (prod.next()) {
-                sum += prod.getInt("preu");
-            }
+
+        } catch (SQLException e) {
+            System.out.println("\nError al introduir la base de dades: " + e.getMessage());
+
         }
         return sum;
     }
 
-    public double suma() throws SQLException {
-        double sum = 0;
-        while (prod.next()) {
-            if (prod.getInt(4) == 1) {
-                sum += prod.getInt("preus");
-            }
+    /**
+     * Inserta un nuevo registro en la tabla 'totals'.
+     *
+     * @param suma valor a insertar en la tabla 'totals'.
+     * @throws SQLException Si ocurre un error al acceder a la base de datos.
+     */
+    public void inserir(int suma) {
+        try {
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            tot = stmt.executeQuery("SELECT * FROM totals");
+
+            tot.moveToInsertRow();
+            tot.updateString("total", String.valueOf(suma));
+            tot.insertRow();
+
+            tot.close();
+        } catch (SQLException e) {
+            System.out.println("\nError al introduir la base de dades: " + e.getMessage());
         }
-        return sum;
+        System.out.println("\nTOTALS s'ha actualitzat amb un total de: " + suma);
     }
 
+    /**
+     * Muestra los productos almacenados en el magatzem.
+     *
+     * @throws SQLException Si ocurre un error al acceder a la base de datos.
+     */
     public void mostrarProductes() {
+        try {
+            Statement stmt = conn.createStatement();
+            prod = stmt.executeQuery("SELECT * FROM productes");
+
+            System.out.println("|----- PRODUCTES DEL MAGATZEM");
+            System.out.printf("| %-6s | %-10s | %-6s | %-6s%n", "ID", "NOM", "PREU", "ACTIU");
+
+            while (prod.next()) {
+                int id = prod.getInt("id");
+                String nom = prod.getString("nom");
+                int preu = prod.getInt("preu");
+                int actiu = prod.getInt("actiu");
+                System.out.printf("| %-6s | %-10s | %-6s | %-6s%n", id, nom, preu, actiu);
+
+            }
+        } catch (SQLException e) {
+            System.out.println("\nError al introduir la base de dades: " + e.getMessage());
+        }
 
     }
 
-    public void inserir(double suma) {
-        
-    }
-
+    /**
+     * Muestra los registros de la tabla 'totals'.
+     *
+     * @throws SQLException Si ocurre un error al acceder a la base de datos.
+     */
     public void mostrarTotals() {
+        try {
+            Statement stmt = conn.createStatement();
+            tot = stmt.executeQuery("SELECT * FROM totals");
+            System.out.println("|----- TAULA TOTALS");
+            System.out.printf("| %-6s | %-10s | %-6s%n", "ID", "DATA", "TOTAL");
 
+            while (tot.next()) {
+                int id = tot.getInt("id");
+                String data = tot.getString("data");
+                int total = tot.getInt("total");
+                System.out.printf("| %-6s | %-10s | %-6s%n", id, data, total);
+
+            }
+        } catch (SQLException e) {
+            System.out.println("\nError al introduir la base de dades: " + e.getMessage());
+        }
     }
 
 }
